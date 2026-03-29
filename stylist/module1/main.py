2d75ad0a470eceefe50196a894a01b51c2384f62
Maussymzhan Makazhan <maussymzhan9@gmail.com>
2026-03-29 14:51:23 +0500

    Fix CORS: add allow_credentials

--- stylist/module1/main.py
+++ stylist/module1/main.py
@@ -27,6 +27,7 @@ app = FastAPI(title="AI Stylist Assistant API", version="2.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
